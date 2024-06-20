3062fce2bb9eafaa4c1b1e8491f3a54a847886db
Natasha Dorogova <natashadorogova@gmail.com>
2024-06-20 23:59:47 +0300

    initial commit

---
+++ sender_stand_request.py
@@ -0,0 +1,70 @@
+# Импорт настроек из модуля configuration, который содержит параметры конфигурации, такие как URL сервиса
+import configuration
+
+# Импорт библиотеки requests для выполнения HTTP-запросов
+import requests
+
+# Импорт данных запроса из модуля data, в котором определены заголовки и тело запроса
+import data
+
+# Определение функции post_new_user для отправки POST-запроса на создание нового пользователя
+def post_new_user(body):
+    # Выполнение POST-запроса с использованием URL из конфигурационного файла, тела запроса и заголовков
+    # URL_SERVICE и CREATE_USER_PATH объединяются для формирования полного URL для запроса
+    # json=body используется для отправки данных пользователя в формате JSON
+    # headers=data.headers устанавливает заголовки запроса из модуля data
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+# Вызов функции post_new_user с телом запроса для создания нового пользователя из модуля data
+response = post_new_user(data.user_body);
+
+# Вывод HTTP-статус кода ответа на запрос
+# Код состояния указывает на результат обработки запроса сервером
+print(response.status_code)
+
+# Определение функции post_new_user для отправки POST-запроса на создание нового пользователя
+def post_new_user(body):
+    # Выполнение POST-запроса с использованием URL из конфигурационного файла, тела запроса и заголовков
+    # URL_SERVICE и CREATE_USER_PATH объединяются для формирования полного URL для запроса
+    # json=body используется для отправки данных пользователя в формате JSON
+    # headers=data.headers устанавливает заголовки запроса из модуля data
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+# Вызов функции post_new_user с телом запроса для создания нового пользователя из модуля data
+response = post_new_user(data.user_body);
+
+# Вывод HTTP-статус кода ответа на запрос
+# Код состояния указывает на результат обработки запроса сервером
+print(response.status_code)
+
+# Функция response.json() позволяет получить тело ответа в формате JSON.
+# Это полезно для извлечения данных, полученных в результате запроса,
+# особенно когда сервер возвращает полезные данные в формате JSON.
+# Здесь мы вызываем эту функцию и выводим полученный JSON в консоль для наглядности.
+print(response.json())
+
+# Импорт необходимых модулей и данных для запроса
+import requests
+import configuration
+import data
+
+# Определение функции для отправки POST-запроса на поиск наборов по продуктам
+def post_products_kits(products_ids):
+    # Отправка POST-запроса с использованием URL из конфигурации, данных о продуктах и заголовков
+    # Возвращается объект ответа, полученный от сервера
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH,
+                         json=products_ids,  # Тело запроса содержит ID продуктов в формате JSON
+                         headers=data.headers)  # Использование заголовков из файла data.py
+
+# Вызов функции с передачей списка ID продуктов из файла data.py
+response = post_products_kits(data.product_ids)
+
+# Вывод HTTP-статус кода ответа и тела ответа в формате JSON
+# Это позволяет проверить успешность выполнения запроса и посмотреть результаты поиска наборов
+print(response.status_code)
+print(response.json())
+
